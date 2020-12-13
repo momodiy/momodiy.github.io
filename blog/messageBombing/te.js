73042668c2b6d57a838f1da1638ddd62d2556cb5
StevenLee <qcjy16@126.com>
2020-12-14 00:10:56 +0800

    :typo: image url typo fix

---
+++ blog/messageBombing/te.js
@@ -0,0 +1,46 @@
+/*
+* Created by Steven Lee
+* Date: 2020/7/18
+* Time: 23:56
+* Project: momodiy.github.io
+* */
+
+var isInterleave = function (s1, s2, s3) {
+  // let index1 = 0
+  // let index2 = 0
+  // let arr1 = []
+  // let arr2 = []
+  // if (s1.length + s2.length !== s3.length) return false
+  //
+  // s3.split('').map((v, i) => {
+  //   if (v === s1.split('')[index1]) {
+  //     index1++
+  //   } else {
+  //     arr1.push(v)
+  //   }
+  //   if (v === s2.split('')[index2]) {
+  //     index2++
+  //   }else {
+  //     arr2.push(v)
+  //   }
+  // })
+  //
+  // let correctLength = index1 === s1.length && index2 === s2.length
+  // let match = arr1.join('') === s2 || arr2.join('') === s1
+  // console.log(correctLength && match)
+  // console.log(arr1.join(''),arr2.join(''))
+  // return correctLength && match
+  let all = []
+
+
+  s1.split('').map((v, i) => {
+
+    // let
+  })
+};
+
+// isInterleave("aabd", "abdc", "aabdbadc")
+isInterleave("aabcc", "dbbca", "a     ")
+
+// bcc dbbca db bc bca c
+
